bb290ba74756f0ab74eb4026209d279531decebd
Seema Sharma <seema.sharma@cern.ch>
2015-04-15 08:20:23 +0200

    added flags to control output size

--- python/HcalTupleMaker_HBHEDigis_cfi.py
+++ python/HcalTupleMaker_HBHEDigis_cfi.py
@@ -3,12 +3,44 @@ import FWCore.ParameterSet.Config as cms
 from SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff import *
 
 hcalTupleHBHEDigis = cms.EDProducer("HcalTupleMaker_HBHEDigis",
-  source  = cms.untracked.InputTag("hcalDigis"),
-  recHits = cms.untracked.InputTag("hbhereco"),
-  Prefix  = cms.untracked.string ( "HBHEDigi"),
-  Suffix  = cms.untracked.string ( ""),
+  source    = cms.untracked.InputTag("hcalDigis"),
+  Prefix    = cms.untracked.string ( "HBHEDigi"),
+  recHits   = cms.untracked.InputTag("hbhereco"),
+  Suffix    = cms.untracked.string ( ""),
   DoChargeReco = cms.untracked.bool ( True ) ,
   DoEnergyReco = cms.untracked.bool ( True ) ,
+  SaveChargeInfo  = cms.untracked.bool ( False ) ,
+  SaveChannelInfo = cms.untracked.bool ( False ) ,
+  SaveDetIdInfo   = cms.untracked.bool ( False ) ,
+  SaveRecHitInfo  = cms.untracked.bool ( False ) ,
+  TotalFCthreshold = cms.untracked.double ( -9999 ) 
+)
+
+hcalTupleHBHEDigisMethod2 = cms.EDProducer("HcalTupleMaker_HBHEDigis",
+  source    = cms.untracked.InputTag("hcalDigis"),
+  Prefix    = cms.untracked.string ( "HBHEDigi"),
+  recHits   = cms.untracked.InputTag("hbhereco"),
+  Suffix    = cms.untracked.string ( ""),
+  DoChargeReco = cms.untracked.bool ( True ) ,
+  DoEnergyReco = cms.untracked.bool ( True ) ,
+  SaveChargeInfo  = cms.untracked.bool ( False ) ,
+  SaveChannelInfo = cms.untracked.bool ( False ) ,
+  SaveDetIdInfo   = cms.untracked.bool ( False ) ,
+  SaveRecHitInfo  = cms.untracked.bool ( False ) ,
+  TotalFCthreshold = cms.untracked.double ( -9999 ) 
+)
+
+hcalTupleHBHEDigisMethod0 = cms.EDProducer("HcalTupleMaker_HBHEDigis",
+  source    = cms.untracked.InputTag("hcalDigis"),
+  Prefix    = cms.untracked.string ( "HBHEDigi"),
+  recHits   = cms.untracked.InputTag("hbhereco"),
+  Suffix    = cms.untracked.string ( ""),
+  DoChargeReco = cms.untracked.bool ( True ) ,
+  DoEnergyReco = cms.untracked.bool ( True ) ,
+  SaveChargeInfo  = cms.untracked.bool ( False ) ,
+  SaveChannelInfo = cms.untracked.bool ( False ) ,
+  SaveDetIdInfo   = cms.untracked.bool ( False ) ,
+  SaveRecHitInfo  = cms.untracked.bool ( False ) ,
   TotalFCthreshold = cms.untracked.double ( -9999 ) 
 )
 
@@ -19,6 +51,7 @@ hcalTupleHBHECosmicsDigis = cms.EDProducer("HcalTupleMaker_HBHEDigis",
   Suffix  = cms.untracked.string ( ""),
   DoChargeReco = cms.untracked.bool ( True ) ,
   DoEnergyReco = cms.untracked.bool ( True ) , 
+  SaveChannelInfo = cms.untracked.bool ( False ) ,
   TotalFCthreshold = cms.untracked.double ( -9999 ) 
 )
 
@@ -29,5 +62,6 @@ hcalTupleHBHEL1JetsDigis = cms.EDProducer("HcalTupleMaker_HBHEDigis",
   Suffix  = cms.untracked.string ( ""),
   DoChargeReco = cms.untracked.bool ( True ) ,
   DoEnergyReco = cms.untracked.bool ( True ) ,
+  SaveChannelInfo = cms.untracked.bool ( False ) ,
   TotalFCthreshold = cms.untracked.double ( -9999 ) 
 )
