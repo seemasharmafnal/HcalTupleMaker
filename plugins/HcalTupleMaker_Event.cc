c8affa19e8a768bcd14a6862cc2156bcbddfdc81
Seema Sharma <seema.sharma@cern.ch>
2015-04-15 08:09:34 +0200

    added orbit,buch

--- plugins/HcalTupleMaker_Event.cc
+++ plugins/HcalTupleMaker_Event.cc
@@ -5,6 +5,8 @@ HcalTupleMaker_Event::HcalTupleMaker_Event(const edm::ParameterSet& iConfig) {
   produces <unsigned int> ( "run"    );
   produces <unsigned int> ( "event"  );
   produces <unsigned int> ( "ls"     );
+  produces <unsigned int> ( "orbit"  );
+  produces <unsigned int> ( "bunch"  );
 }
 
 void HcalTupleMaker_Event::
@@ -13,9 +15,13 @@ produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   std::auto_ptr<unsigned int >  run   ( new unsigned int(iEvent.id().run()        ) );
   std::auto_ptr<unsigned int >  event ( new unsigned int(iEvent.id().event()      ) );
   std::auto_ptr<unsigned int >  ls    ( new unsigned int(iEvent.luminosityBlock() ) );
+  std::auto_ptr<unsigned int >  orbit ( new unsigned int(iEvent.orbitNumber()     ) );
+  std::auto_ptr<unsigned int >  bunch ( new unsigned int(iEvent.bunchCrossing()   ) );
 
   iEvent.put( run,   "run"   );
   iEvent.put( event, "event" );
   iEvent.put( ls   , "ls"    );
+  iEvent.put( orbit, "orbit"    );
+  iEvent.put( bunch, "bunch"    );
 
 }
